62ac12b11c422a13ef9af473353cd00eff9b315a
FrankN0 <frankniroysiahaan1@gmail.com>
2024-01-15 12:51:16 +0700

    t00

--- t00_01.c
+++ t00_01.c
@@ -1,9 +1,9 @@
-// NIM - NAMA
+// 12S23016 - Frank Niroy Siahaan
 
 #include <stdio.h>
 
 int main(char **_argc, int _argv)
 {
-    // Show your code here ...
+    printf("Hello, World!");
     return 0;
 }
